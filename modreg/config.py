91f3d4d24d54f59d15c04242709dafd963e3b978
isbobby <bobbyclex8@gmail.com>
2019-11-12 03:16:06 +0800

    deployment commit

--- modreg/config.py
+++ modreg/config.py
@@ -3,6 +3,6 @@ import os
 #config database uri here
 class Config:
     SECRET_KEY = 'c9970460fc2c3ad324add53c94e3bc2a'
-    SQLALCHEMY_DATABASE_URI = 'postgres://hlwsozdbaciqel:fe05139be83c565210c53a5a48d097f602de5ab21b43bcdea8ab9e9e6dce855f@ec2-107-22-160-102.compute-1.amazonaws.com:5432/dmj6idt7voapg'
+    SQLALCHEMY_DATABASE_URI = 'postgres://vztzvvjrtwvstn:cab4c8cf60f45147554123154c162f980b274f30ed041f8b46ff7eaa03a80615@ec2-174-129-194-188.compute-1.amazonaws.com:5432/d6om5mduqv2h7s'
     USER_APP_NAME = "CS2102 Project"      # Shown in and email templates and page footers
     USER_EMAIL_SENDER_EMAIL = "somedummy@gmail.com" #not used but required to initialize app
